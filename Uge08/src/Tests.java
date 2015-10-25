da6de449df99ee0ed0ae15055e8945408af16812
cbudtz <chrbudtz@gmail.com>
2015-10-25 15:28:05 +0100

    ArrayUtils

---
+++ Uge08/src/Tests.java
@@ -0,0 +1,16 @@
+
+
+
+public class Tests {
+
+	public static void main(String[] args) {
+		///Test
+		int[] a = {1,3,3,2,1};
+		System.out.println("As string: " + ArrayUtils.arrayToString(a));
+		System.out.println("Occurences: " + ArrayUtils.occurences(a, 3));
+		System.out.println("Sum: " + ArrayUtils.sum(a));		
+		///EOT
+
+	}
+
+}
